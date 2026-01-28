20c27f38eaaa974e56a0a274645983b9478ecf20
Ananthu s <157121772+ATU-S@users.noreply.github.com>
2026-01-28 20:39:41 +0530

    2nd

---
+++ src/schema.py
@@ -0,0 +1,16 @@
+BEHAVIORAL_FEATURES = [
+    "screen_time",
+    "phone_unlocks",
+    "night_usage",
+    "social_media_hours",
+    "gaming_hours",
+    "notifications"
+]
+
+PSYCHOLOGICAL_FEATURES = [
+    "stress_level",
+    "anxiety_score",
+    "sleep_quality",
+    "self_control",
+    "loneliness"
+]
